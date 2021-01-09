605b937fc11cbdeb9a3a12b3e1ee05e1fac7f5b9
Marvin Rudolph <info@marvin-rudolph.de>
2021-01-09 21:16:36 +0100

    test: use slash for paths

--- packages/reporter-console/src/utils/error.ts
+++ packages/reporter-console/src/utils/error.ts
@@ -7,7 +7,15 @@ export function parseStack(stack: string) {
   const lines = stack
     .split('\n')
     .splice(1)
-    .map((l) => l.trim().replace('file://', '').replace(cwd, ''))
+    .map((l) => {
+      const line = l.trim().replace('file://', '').replace(cwd, '')
+
+      if (process.env.NODE_ENV === 'test') {
+        return line.replace(/\\/g, '/')
+      }
+
+      return line
+    })
 
   return lines
 }
