22d2cb7283093107d724035a2a7800f86b457307
Marvin Rudolph <rudolph.marvin@web.de>
2021-09-27 11:13:51 +0200

    refactor(reporter-console): use correct console method

--- packages/reporter-console/src/reporter.ts
+++ packages/reporter-console/src/reporter.ts
@@ -1,11 +1,14 @@
-import type { Reporter, ReporterOptions } from 'loglin'
+import { Reporter, ReporterOptions, LogLevel } from 'loglin'
 import { basicFormatter } from './formatter'
 
 export function consoleReporter(options?: ReporterOptions): Reporter {
   const formatter = options?.formatter || basicFormatter()
 
   return {
-    log: (info) => console.log(formatter(info)),
+    log: (info) =>
+      info.level !== LogLevel.Fatal && info.level !== LogLevel.Success
+        ? console[info.level](formatter(info))
+        : console.log(formatter(info)),
     filters: options?.filters
   }
 }
